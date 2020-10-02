e2a1bfc9aeb07919f4d4b84268483072f49cac42
DrAllStar16 <alexander.somich@gmail.com>
2020-10-02 16:51:20 -0700

    Finished Backwards string started mixed caps

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -10,12 +10,8 @@ public class BackwardsString extends SpecialString {
 	public String funkifyText(String s) {
 		String d = new String();
 		Character t;
-		int e = 0;
-		for (int i = s.length() - 1; i >= 0; i--) {
-			t = s.charAt(i);
-			d.charAt(e)
-		}
-		return "";
+	
+		return d;
 	}
 
 }
--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -8,8 +8,22 @@ public class MixedCapsString extends SpecialString{
 
 	@Override
 	public String funkifyText(String s) {
+		String d = new String();
+		Character t;
+		Character z;
+		for (int i = 0; i < 1; i++) {
+			z = s.charAt(i);
+			z.toUpperCase(i);
+			d = d +z;
+		}
 		
-		return "";
+		for (int i = 1; i < s.length(); i++) {
+			z = s.charAt(i);
+			z.toLowerCase(i);
+			d = d +z;
+		}
+		
+		return d;
 	}
 
 }
