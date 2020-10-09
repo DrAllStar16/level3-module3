f8c5ace0fffbf79e2a23f7b2bf49e54a1e417356
DrAllStar16 <alexander.somich@gmail.com>
2020-10-09 16:54:31 -0700

    Worked on backwards string

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -10,7 +10,13 @@ public class BackwardsString extends SpecialString {
 	public String funkifyText(String s) {
 		String d = new String();
 		Character t;
-	
+		
+		
+		int e = 0;
+		for (int i = s.length(); i > 0; i--) {
+			t = s.charAt(i);
+			
+		}
 		return d;
 	}
 
