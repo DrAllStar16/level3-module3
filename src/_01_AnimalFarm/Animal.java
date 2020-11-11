29edcf37b16af9ed04eeb809ba3983eaa28333cb
DrAllStar16 <alexander.somich@gmail.com>
2020-11-10 16:56:04 -0800

    Worked on Animal Farm

--- src/_01_AnimalFarm/Animal.java
+++ src/_01_AnimalFarm/Animal.java
@@ -2,8 +2,12 @@ package _01_AnimalFarm;
 
 public abstract class Animal {
 
-	public abstract int sumOfTwo(int n1, int n2);
+	public abstract int makeNoise(int n1, int n2);
 
+	public int common() {
+		return 0;
+		
+	}
 	
 	
 	
---
+++ src/_01_AnimalFarm/Cow.java
@@ -0,0 +1,11 @@
+package _01_AnimalFarm;
+
+public class Cow extends Animal{
+
+	@Override
+	public int makeNoise(int n1, int n2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
