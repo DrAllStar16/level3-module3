023b36a7ba777262be63d2732e27e9c1bfca4ce6
DrAllStar16 <alexander.somich@gmail.com>
2020-11-24 16:48:34 -0800

    Worked on Farm

--- src/_01_AnimalFarm/Farm.java
+++ src/_01_AnimalFarm/Farm.java
@@ -5,7 +5,16 @@ import java.util.ArrayList;
 public class Farm {
 
 	public static void main(String[] args) {
+		Cow cow = new Cow();
+		Pig pig = new Pig();
+		Chicken chicken = new Chicken();
+		Sheep sheep = new Sheep();
 		ArrayList<Animal> Farm = new ArrayList<Animal>();
+		
+		Farm.add(cow);
+		Farm.add(pig);
+		Farm.add(chicken);
+		Farm.add(sheep);
 		
 		
 	
